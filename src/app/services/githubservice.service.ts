8967728cfbf63e9012f42ac46d76fb56daa58a55
debbyne <kinyadebbyne@gmail.com>
2022-02-14 09:09:34 +0300

    add changes to style

--- src/app/services/githubservice.service.ts
+++ src/app/services/githubservice.service.ts
@@ -1,39 +1,48 @@
 import { Repository } from './../repository';
 import { User } from './../user';
 import { Injectable } from '@angular/core';
-import { HttpClient,HttpHeaders} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GithubserviceService {
-  user!:User
-  repo!:Repository
+  user!: User;
+  repo!: Repository;
 
-  constructor(private http:HttpClient) {
-    this.user = new User('',0)
-    this.repo = new Repository('',new Date())
-   }
+  constructor(private http: HttpClient) {
+    this.user = new User('', 0, '', '');
+    this.repo = new Repository('', new Date());
+  }
 
-     // handle a user name input
-  findUser(whatToSearch:any){
-    interface GithubAPI{
-      login:string,public_repos:number
-    
+  // handle a user name input
+  findUser(whatToSearch: any) {
+    interface GithubAPI {
+      login: string;
+      public_repos: number;
+      public_bio: string;
+      avatar_url: string;
     }
-    let headers = new HttpHeaders({'Authorization':'token' + environment.api_key})
-    let request = environment.baseUrl + whatToSearch ;
-    let params={headers:headers}
+    let headers = new HttpHeaders({
+      Authorization: 'token' + environment.api_key,
+    });
+    let request = environment.baseUrl + whatToSearch;
+    let params = { headers: headers };
     let promise = new Promise((resolve, reject) => {
-      this.http.get<GithubAPI>(request,params).toPromise().then((response: any) => { this.user = response;
-            
+      this.http
+        .get<GithubAPI>(request, params)
+        .toPromise()
+        .then(
+          (response: any) => {
+            this.user = response;
+
             resolve(resolve);
-            console.log(this.user)
+            console.log(this.user);
           },
-          (error:any) => {
+          (error: any) => {
             reject();
-            console.log(error)
+            console.log(error);
           }
         );
     });
@@ -41,26 +50,30 @@ export class GithubserviceService {
   }
   userRepos(githubSearch: any) {
     interface GithubRepo {
-      html_url: string,
+      html_url: string;
     }
 
-    let headers = new HttpHeaders({'Authorization': 'token' + environment.api_key})
-    let request = environment.baseUrl + githubSearch  + '/repos';;
-    let params = {headers: headers}
+    let headers = new HttpHeaders({
+      Authorization: 'token' + environment.api_key,
+    });
+    let request = environment.baseUrl + githubSearch + '/repos';
+    let params = { headers: headers };
     let promise = new Promise((resolve, reject) => {
-      this.http.get<GithubRepo>(request, params).toPromise().then((response: any) =>{
-        this.repo = response;
-        resolve(resolve);
-      },
-      (error: any) => {
-        reject();
-        console.log(error)
-      }
-    );
+      this.http
+        .get<GithubRepo>(request, params)
+        .toPromise()
+        .then(
+          (response: any) => {
+            this.repo = response;
+            resolve(resolve);
+          },
+          (error: any) => {
+            reject();
+            console.log(error);
+          }
+        );
     });
 
     return promise;
   }
-
-
 }
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,4 +1,8 @@
 export class User {
-    constructor(public login:string, public public_repos:number
+    constructor(public login:string, 
+        public public_repos:number,
+        public public_bio:string,
+        public avatar_url:string,
+
         ){}
 }
